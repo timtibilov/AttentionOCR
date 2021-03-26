6c5202de07bf39c7efe6e27c684b4eaf4f75a06b
t.tibilov <t.tibilov@macbook-t.tibilov>
2021-03-26 10:07:29 +0300

    dev-3 #5 realized forward for CNN^ tests needed

--- src/model/cnn.py
+++ src/model/cnn.py
@@ -7,13 +7,12 @@ import torch.nn.functional as F
 from typing import Optional, List
 
 
-def conv3x3(in_planes: int, out_planes: int, stride: int = 1, groups: int = 1, dilation: int = 1) -> nn.Conv2d:
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+def conv3x3(input_size: int, output_size: int, stride: int = 1) -> nn.Conv2d:
+    return nn.Conv2d(input_size, output_size, kernel_size=3, stride=stride, padding=1, bias=False)
 
 
-def conv1x1(in_planes: int, out_planes: int, stride: int = 1) -> nn.Conv2d:
-    return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False)
+def conv1x1(input_size: int, output_size: int, stride: int = 1) -> nn.Conv2d:
+    return nn.Conv2d(input_size, output_size, kernel_size=1, stride=stride, bias=False)
 
 
 class BasicBlock(nn.Module):
@@ -67,32 +66,40 @@ class CNN(nn.Module):
 
         self.relu = nn.ReLU()
         self.output = output_size
-        self.inplanes = 128
+        self.input_size = 128
         self.layer0 = nn.Sequential(
-            nn.Conv2d(1, self.inplanes, kernel_size=7, stride=2, padding=3),
-            nn.BatchNorm2d(self.inplanes),
+            nn.Conv2d(1, self.input_size, kernel_size=7, stride=2, padding=3),
+            nn.BatchNorm2d(self.input_size),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
         self.layer1 = self._make_layer(128, layers[0])
-        self.layer2 = self._make_layer(256, layers[1])
-        self.layer3 = self._make_layer(512, layers[2])
-        self.layer4 = self._make_layer(512, layers[3])
+        self.layer2 = self._make_layer(256, layers[1], stride=2)
+        self.layer3 = self._make_layer(512, layers[2], stride=2)
+        self.layer4 = self._make_layer(512, layers[3], stride=2)
         self.downsample = conv1x1(512, self.output)
 
-    def _make_layer(self, planes: int, blocks: int, stride: int = 1) -> nn.Sequential:
+    def _make_layer(self, output_size: int, blocks: int, stride: int = 1) -> nn.Sequential:
         downsample = None
-        if stride != 1 or self.inplanes != planes:
+        if stride != 1 or self.input_size != output_size:
             downsample = nn.Sequential(
-                conv1x1(self.inplanes, planes, stride),
-                nn.BatchNorm2d(planes),
+                conv1x1(self.input_size, output_size, stride),
+                nn.BatchNorm2d(output_size),
             )
-        layers = [
-            BasicBlock(self.inplanes, planes, stride, downsample)
-        ]
-        self.inplanes = planes
+        layers = [BasicBlock(self.input_size, output_size, stride, downsample)]
+
+        self.input_size = output_size
 
         for _ in range(1, blocks):
-            layers.append(BasicBlock(self.inplanes, planes))
-        
-        return layers
+            layers.append(BasicBlock(self.input_size, output_size))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.layer0(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.downsample(x)
+        return x
